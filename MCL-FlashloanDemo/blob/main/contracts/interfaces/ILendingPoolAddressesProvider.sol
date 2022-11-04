0624cc532ec9929598093b9cff9aca14ec3e74e4
yunfengflashloan <116545884+yunfengflashloan@users.noreply.github.com>
2022-11-05 01:49:13 +0800

    Update ILendingPoolAddressesProvider.sol

--- MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPoolAddressesProvider.sol
+++ MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPoolAddressesProvider.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.0;
- 
- 
-interface IPancakeCalleeB {
+
+contract IPancakeCalleeB {
     function pancakeCall03(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
